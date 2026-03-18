1b13a2d906497621d2a79587088cc0381907347c
sai <lequynga12345@gmail.com>
2026-03-18 21:34:35 +0700

    Web sever and basic send functions

--- src/main.cpp
+++ src/main.cpp
@@ -1,94 +1,205 @@
 /* Copyright 2019 David Conran
-*
-* An IR LED circuit *MUST* be connected to the ESP8266 on a pin
-* as specified by kIrLed below.
-*
-* TL;DR: The IR LED needs to be driven by a transistor for a good result.
-*
-* Suggested circuit:
-*     https://github.com/crankyoldgit/IRremoteESP8266/wiki#ir-sending
-*
-* Common mistakes & tips:
-*   * Don't just connect the IR LED directly to the pin, it won't
-*     have enough current to drive the IR LED effectively.
-*   * Make sure you have the IR LED polarity correct.
-*     See: https://learn.sparkfun.com/tutorials/polarity/diode-and-led-polarity
-*   * Typical digital camera/phones can be used to see if the IR LED is flashed.
-*     Replace the IR LED with a normal LED if you don't have a digital camera
-*     when debugging.
-*   * Avoid using the following pins unless you really know what you are doing:
-*     * Pin 0/D3: Can interfere with the boot/program mode & support circuits.
-*     * Pin 1/TX/TXD0: Any serial transmissions from the ESP8266 will interfere.
-*     * Pin 3/RX/RXD0: Any serial transmissions to the ESP8266 will interfere.
-*   * ESP-01 modules are tricky. We suggest you use a module with more GPIOs
-*     for your first time. e.g. ESP-12 etc.
-*/
+ *
+ * This example code demonstrates how to use the "Common" IRac class to control
+ * various air conditions. The IRac class does not support all the features
+ * for every protocol. Some have more detailed support that what the "Common"
+ * interface offers, and some only have a limited subset of the "Common" options.
+ *
+ * This example code will:
+ * o Try to turn on, then off every fully supported A/C protocol we know of.
+ * o It will try to put the A/C unit into Cooling mode at 25C, with a medium
+ *   fan speed, and no fan swinging.
+ * Note: Some protocols support multiple models, only the first model is tried.
+ *
+ */
 #include <Arduino.h>
 #include <IRremoteESP8266.h>
-#include <IRsend.h>
-#include <ir_Samsung.h>
+#include <IRac.h>
+#include <IRutils.h>
 
-const uint16_t kIrLed = 4;  // ESP8266 GPIO pin to use. Recommended: 4 (D2).
-IRSamsungAc ac(kIrLed);     // Set the GPIO used for sending messages.
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+#include <ESP8266WebServer.h>
+#include <ESP8266mDNS.h>
 
-void printState() {
-  // Display the settings.
-  Serial.println("Samsung A/C remote is in the following state:");
-  Serial.printf("  %s\n", ac.toString().c_str());
-}
+#include <main_private.h>
 
-void setup() {
-  ac.begin();
-  Serial.begin(115200);
+/*WIFI settings*/
+
+IRac ac(IR_REMOTE_KELON168_GPIO_PIN); // Create a A/C object using GPIO to sending messages with.
+
+ESP8266WebServer server(80);
+
+void IR_setup();
+void WIFI_setup();
+void WIFI_loop();
+void WIFI_handleRoot();
+void WIFI_handleNotFound();
+
+void IR_setup()
+{
   delay(200);
-
-  // Set up what we want to send. See ir_Samsung.cpp for all the options.
-  Serial.println("Default state of the remote.");
-  printState();
-  Serial.println("Setting initial state for A/C.");
-  ac.off();
-  ac.setFan(kSamsungAcFanLow);
-  ac.setMode(kSamsungAcCool);
-  ac.setTemp(25);
-  ac.setSwing(false);
-  printState();
+  /*Setup the A/C specifications*/
+  ac.next.protocol = decode_type_t::KELON168;    // Set a protocol to use.
+  ac.next.mode = stdAc::opmode_t::kCool;         // Run in cool mode initially.
+  ac.next.celsius = true;                        // Use Celsius for temp units. False = Fahrenheit
+  ac.next.degrees = 25;                          // 25 degrees.
+  ac.next.fanspeed = stdAc::fanspeed_t::kMedium; // Start the fan at medium.
+  ac.next.swingv = stdAc::swingv_t::kOff;        // Don't swing the fan up or down.
+  ac.next.swingh = stdAc::swingh_t::kOff;        // Don't swing the fan left or right.
+  ac.next.light = false;                         // Turn off any LED/Lights/Display that we can.
+  ac.next.beep = false;                          // Turn off any beep from the A/C if we can.
+  ac.next.econo = false;                         // Turn off any economy modes if we can.
+  ac.next.filter = false;                        // Turn off any Ion/Mold/Health filters if we can.
+  ac.next.turbo = false;                         // Don't use any turbo/powerful/etc modes.
+  ac.next.quiet = false;                         // Don't use any quiet/silent/etc modes.
+  ac.next.sleep = -1;                            // Don't set any sleep time or modes.
+  ac.next.clean = false;                         // Turn off any Cleaning options if we can.
+  ac.next.clock = -1;                            // Don't set any current time if we can avoid it.
+  ac.next.power = false;                         // Initially start with the unit off.
+  Serial.println("Try to turn on & off every supported A/C type ...");
 }
 
-void loop() {
-  // Turn the A/C unit on
-  Serial.println("Turn on the A/C ...");
-  ac.on();
-  ac.send();
-  printState();
-  delay(15000);  // wait 15 seconds
-  // and set to cooling mode.
-  Serial.println("Set the A/C mode to cooling ...");
-  ac.setMode(kSamsungAcCool);
-  ac.send();
-  printState();
-  delay(15000);  // wait 15 seconds
+void WIFI_setup()
+{
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  Serial.println("WiFi connecting...");
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
 
-  // Increase the fan speed.
-  Serial.println("Set the fan to high and the swing on ...");
-  ac.setFan(kSamsungAcFanHigh);
-  ac.setSwing(true);
-  ac.send();
-  printState();
-  delay(2000);
+  if (MDNS.begin("esp8266"))
+  {
+    Serial.println("MDNS responder started");
+  }
 
-  // Change to Fan mode, lower the speed, and stop the swing.
-  Serial.println("Set the A/C to fan only with a low speed, & no swing ...");
-  ac.setSwing(false);
-  ac.setMode(kSamsungAcFan);
-  ac.setFan(kSamsungAcFanLow);
-  ac.send();
-  printState();
-  delay(15000);
+  server.on("/", WIFI_handleRoot);
+  server.onNotFound(WIFI_handleNotFound);
+  server.begin();
+  Serial.println("HTTP server started");
 
-  // Turn the A/C unit off.
-  Serial.println("Turn off the A/C ...");
-  ac.off();
-  ac.send();
-  printState();
-  delay(15000);  // wait 15 seconds
+  server.on("/tempup", []()
+            {
+  Serial.println("tempup");
+  ac.next.degrees = min(ac.next.degrees + 1, 30.0f); // Max 30C
+  ac.sendAc();
+  server.send(200, "text/plain", "OK"); });
+
+  server.on("/tempdown", []()
+            {
+    Serial.println("tempdown");
+    ac.next.degrees = max(ac.next.degrees - 1, 16.0f); // Min 16C
+    ac.sendAc();
+    server.send(200, "text/plain", "OK"); });
+
+  server.on("/togglepower", []()
+            {
+    Serial.println("togglepower");
+    ac.next.power = !ac.next.power;
+    ac.sendAc();
+    server.send(200, "text/plain", "OK"); });
+
+  server.on("/modecool", []()
+            {
+    Serial.println("modecool");
+    ac.next.mode = stdAc::opmode_t::kCool;
+    ac.sendAc();
+    server.send(200, "text/plain", "OK"); });
+
+  server.on("/modefan", []()
+            {
+    Serial.println("modefan");
+    ac.next.mode = stdAc::opmode_t::kFan;
+    ac.sendAc();
+    server.send(200, "text/plain", "OK"); });
+}
+
+void setup()
+{
+  Serial.begin(115200);
+  IR_setup();
+  WIFI_setup();
+}
+
+void loop()
+{
+  WIFI_loop();
+  MDNS.update();
+}
+void WIFI_handleRoot()
+{
+  String html = F("<!DOCTYPE html><html><head>");
+  html += F("<meta name='viewport' content='width=device-width, initial-scale=1'>");
+  html += F("<style>");
+  html += F("body { font-family: sans-serif; background: #f0f2f5; text-align: center; color: #333; }");
+  html += F(".card { background: white; margin: 20px auto; padding: 20px; border-radius: 15px; max-width: 400px; box-shadow: 0 4px 10px rgba(0,0,0,0.1); }");
+  html += F(".temp-display { font-size: 48px; font-weight: bold; margin: 10px 0; color: #007bff; }");
+  html += F(".btn { background: #007bff; border: none; color: white; padding: 15px 25px; border-radius: 10px; font-size: 18px; margin: 5px; cursor: pointer; width: 40%; transition: 0.3s; }");
+  html += F(".btn:active { transform: scale(0.95); background: #0056b3; }");
+  html += F(".btn-pwr { background: #dc3545; width: 90%; }");
+  html += F(".grid { display: flex; flex-wrap: wrap; justify-content: center; }");
+  html += F("</style></head><body>");
+
+  html += F("<div class='card'>");
+  html += F("<h2>AC Control</h2>");
+
+  // Temperature Section
+  html += F("<div class='temp-display'><span id='temp'>");
+  html += String((int)ac.next.degrees);
+  html += F("</span>&deg;C</div>");
+
+  html += F("<div class='grid'>");
+  html += F("<button class='btn' onclick='sendCmd(\"tempdown\")'>-</button>");
+  html += F("<button class='btn' onclick='sendCmd(\"tempup\")'>+</button>");
+  html += F("</div>");
+
+  html += F("<hr style='border:0; border-top:1px solid #eee; margin:20px 0;'>");
+
+  // Power and Mode
+  html += F("<button class='btn btn-pwr' onclick='sendCmd(\"togglepower\")'>ON / OFF</button>");
+
+  html += F("<div class='grid' style='margin-top:10px;'>");
+  html += F("<button class='btn' style='background:#17a2b8' onclick='sendCmd(\"modecool\")'>Cool</button>");
+  html += F("<button class='btn' style='background:#ffc107; color:black' onclick='sendCmd(\"modefan\")'>Fan</button>");
+  html += F("</div>");
+
+  html += F("</div>"); // End Card
+
+  // AJAX Script
+  html += F("<script>");
+  html += F("function sendCmd(path) {");
+  html += F("  fetch('/' + path).then(response => {");
+  html += F("    if(path.includes('temp')) { location.reload(); }"); // Reload to see new temp
+  html += F("  });");
+  html += F("}");
+  html += F("</script></body></html>");
+
+  server.send(200, "text/html", html);
+}
+void WIFI_handleNotFound()
+{
+  String message = "File Not Found\n\n";
+  message += "URI: ";
+  message += server.uri();
+  message += "\nMethod: ";
+  message += (server.method() == HTTP_GET) ? "GET" : "POST";
+  message += "\nArguments: ";
+  message += server.args();
+  message += "\n";
+  for (uint8_t i = 0; i < server.args(); i++)
+  {
+    message += " " + server.argName(i) + ": " + server.arg(i) + "\n";
+  }
+  server.send(404, "text/plain", message);
+}
+
+void WIFI_loop()
+{
+  server.handleClient();
 }
---
+++ src/main_private.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_PRIVATE_H
+#define MAIN_PRIVATE_H
+
+
+#define IR_REMOTE_KELON168_GPIO_PIN 4  // GPIO pin to use for sending IR messages for Kelon168 A/Cs.
+
+
+/****************WIFI SETTINGS**************** */
+#define WIFI_SSID "Metisai"
+#define WIFI_PASSWORD "Lequynga1@"
+
+#endif // MAIN_PRIVATE_H
